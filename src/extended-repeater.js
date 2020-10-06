25dc92d8639600a2a35d2fea1a990e4d5b92fa51
Abdulloh Musayev <musayevabdulloh76@gmail.com>
2020-10-06 17:15:59 +0500

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const separatorDefault = '+';
+const additionSeparatorDefault = '|';
+
+module.exports = function repeater(str, options) {
+  
+  if(!options.separator)
+    options.separator = separatorDefault;
+  if(!options.additionSeparator)
+    options.additionSeparator = additionSeparatorDefault;
+
+
+  const additional = () => {
+    let add = options.addition;
+    for(let i = 0; i < options.additionRepeatTimes - 1; i++) {
+      add += options.additionSeparator + options.addition;
+    }
+  return add;
+  // add -> PLUS00PLUS00PLUS
+  }
+
+  if(str === null)
+    str = 'null';
+  if(options.addition === null)
+    options.addition = 'null';
+  
+  str = str.toString();
+  let result = str;
+  let repeatedText = str;
+  if(options.addition || options.addition === false) {
+    options.addition = options.addition.toString();
+    result += additional();
+    repeatedText += additional(); // repeating text 
+  }
+  for(let i = 0; i < options.repeatTimes-1; i++) {
+    result += options.separator + repeatedText;
+    // result += options.addition ? options.separator + str + additional() : options.separator + str;
+  }
+
+  return result;
+
 };
   
