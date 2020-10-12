c63ec202ab9b9c2cf0bae48a49283360bc051226
Abdulloh Musayev <musayevabdulloh76@gmail.com>
2020-10-12 07:56:48 +0500

    feat: implement depth calculating method

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,24 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	calculateDepth(arr) {
+		let previous, current;
+		
+		// calculate depth of elements from last to first
+		current =
+			arr[arr.length - 1].length === undefined || typeof arr[arr.length - 1] === 'string'
+				? 0
+				: arr[arr.length - 1].length === 0 ? 1 : this.calculateDepth(arr[arr.length - 1]);
+
+		for (let i = arr.length - 1; i > 0; i--) {
+			previous =
+				arr[i - 1].length === undefined || typeof arr[i - 1] === 'string'
+					? 0
+					: arr[i - 1].length === 0 ? 1 : this.calculateDepth(arr[i - 1]);
+
+			current = previous > current ? previous : current;
+		}
+
+		return current + 1;
+	}
 };
